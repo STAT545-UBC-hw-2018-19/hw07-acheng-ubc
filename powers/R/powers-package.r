3e4e43c5495c5358d34917972483cd6c078bd361
Alex Cheng <alex.cheng@alumni.ubc.ca>
2018-11-15 21:30:03 -0800

    edit ?powers documentation

--- powers/R/powers-package.r
+++ powers/R/powers-package.r
@@ -1,11 +1,15 @@
-#' Convenient Computation of Powers
+#' Convenient Computation of Powers and Box Cox Transformation
 #'
 #' Are you tired of using the power operator, \code{^} or \code{**} in R?
 #' Use this package to call functions that apply common powers
 #' to your vectors.
+#' 
+#' Is your data suffering from non-normality or heteroscedasticity? Fear not, use
+#' the Box-Cox transformation function bc_transform() in this package to lower the
+#' variance and normalize your data!
 #'
 #' @name powers
-#' @author Vincenzo Coia
+#' @author Vincenzo Coia, Alex Cheng
 #' @note This package isn't actually meant to be serious. It's just for
 #' teaching purposes.
 #' @docType package
