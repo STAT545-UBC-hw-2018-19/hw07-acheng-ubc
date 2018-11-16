ee530166947528afc1fd6025308ee8542fd68323
Alex Cheng <alex.cheng@alumni.ubc.ca>
2018-11-15 23:26:11 -0800

    added right-skewed data skewvec

---
+++ powers/R/skewvec.R
@@ -0,0 +1,8 @@
+#' Right-skewed vector of numbers
+#'
+#' Numbers are: -5.0 -4.0 -6.0 -2.0  0.0  1.0  2.2  1.5  1.0  2.0  1.0
+#' 4.0  2.0  2.0  1.0  0.0  0.0  0.0  1.1  3.2  4.0  3.0  2.0  9.9 10.6
+#' 16.9 21.0 23.0 27.0 39.0
+#' @format This (and the next) is a new tag that applies for data documentation.
+#' @source Note that you shouldn't use the `@export` tag when documenting data!
+"skewvec"
