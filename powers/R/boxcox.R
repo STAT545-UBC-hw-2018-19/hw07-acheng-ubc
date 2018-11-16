708d2d94d7fbe7347aabda69650f3ddbebab2f46
Alex Cheng <alex.cheng@alumni.ubc.ca>
2018-11-15 23:25:47 -0800

    investigated error with plotit default value (could not reproduce error????)

--- powers/R/boxcox.R
+++ powers/R/boxcox.R
@@ -9,16 +9,17 @@
 #' @examples
 #' bc_transform(tenvec, 0)
 #' bc_transform(-5:5, 1)
+#' bc_transform(skewvec)
 #' 
-bc_transform <- function(x, lambda = 0){
+bc_transform <- function(x, lambda=0, plot_it=FALSE){
   # check if x is numeric
   if(any(!is.numeric(x))){
     stop("Box-Cox transformation requires a vector of numbers as an input")
   }
   
-  # if any value in x is negative, add delta = |min| + 1% to assure all values are positive
+  # if any value in x is negative, add delta = |min| + 10% to assure all values are positive
   if(any(x <= 0)){
-    delta = abs(min(x)) + 0.01 * abs(min(x))
+    delta = abs(min(x)) + 0.1 * abs(min(x))
     x <- x + delta
   }
   
@@ -29,7 +30,8 @@ bc_transform <- function(x, lambda = 0){
   }
   
   # perform transformation
-  res <- (pow(x, lambda) - 1)/lambda
+  res <- (pow(x, lambda, plot_it) - 1)/lambda
   return(res)
-
 }
+
+
