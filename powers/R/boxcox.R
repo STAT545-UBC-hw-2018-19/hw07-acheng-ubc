66627d004e977c8804639fb00c987740efdd600e
Alex Cheng <alex.cheng@alumni.ubc.ca>
2018-11-15 20:54:57 -0800

    created box cox function bc_transform()

---
+++ powers/R/boxcox.R
@@ -0,0 +1,35 @@
+#' Perform a Box-Cox transformation
+#'
+#' @param x Vector of data points
+#' @param lambda Lambda value to be inputted manually or estimated using bc_lambda()
+#'
+#' @return Vector of transformed data points
+#' @export
+#'
+#' @examples
+#' bc_transform(tenvec, 0)
+#' bc_transform(-5:5, 1)
+#' 
+bc_transform <- function(x, lambda = 0){
+  # check if x is numeric
+  if(any(!is.numeric(x))){
+    stop("Box-Cox transformation requires a vector of numbers as an input")
+  }
+  
+  # if any value in x is negative, add delta = |min| + 1% to assure all values are positive
+  if(any(x <= 0)){
+    delta = abs(min(x)) + 0.01 * abs(min(x))
+    x <- x + delta
+  }
+  
+  # exception for lambda = 0
+  if(lambda == 0){
+    res <- log(x) # natural logarithm
+    return(res)
+  }
+  
+  # perform transformation
+  res <- (pow(x, lambda) - 1)/lambda
+  return(res)
+
+}
